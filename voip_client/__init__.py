c0be2a8afdee485eeb994b4af98581aedb54e23b
Sergey004 <sergey00475@gmail.com>
2025-09-27 16:50:46 +0400

    BYE MY OWN LIB!

--- voip_client/__init__.py
+++
@@ -1,1 +0,0 @@
-# Package initialization for voip_client
--- voip_client/voip_client/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-VoIP Client Package
-Initializes the VoIP client package with core components.
-"""
-
-from .voip import VoIPClient, CallState
